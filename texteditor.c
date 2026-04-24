956ae364e5adb07fd8d6a79cc78677a0121c93eb
umeshhnandavja-byte <umeshhnanda.vja@gmail.com>
2026-04-24 14:00:44 +0530

    Raw Mode

--- texteditor.c
+++ texteditor.c
@@ -1,3 +1,40 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <termios.h>
+
+struct termios orig_termios;
+
+void disableRawMode(){
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode(){
+
+    struct termios raw = orig_termios;
+
+    tcgetattr(STDIN_FILENO, &raw);
+
+    raw.c_lflag &= ~(ECHO | ICANON);
+
+    atexit(disableRawMode);
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
 int main(){
+
+    enableRawMode();
+
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
+        if(iscntrl(c)){
+            printf("%d\n", c);
+        }
+        else{
+            printf("%d ('%c')\n");
+        }
+    }
     return 0;
 }
